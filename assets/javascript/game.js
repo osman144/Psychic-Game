2d5506838bc6f6bc1b975860b28e7ced6ad81dc5
osman144 <osman144@umn.edu>
2018-07-15 17:24:00 -0500

    randomizer function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 'use strict'; 
 
 // 15 Flags Each
-const wordGuess = [
+const flagGuess = [
     {name:'Andorra',
     flagImage: 'http://flags.fmcdn.net/data/flags/w580/ad.png'
     },
@@ -55,21 +55,52 @@ let guesses = 9;
 let soFar = [];
 let userGuess;
 let computerChoice;
+let previousFlag = [];
 
 // DOM manipulation
 function gameReset(){
-    wins = 0;
-    losses = 0;
-    guesses = 9;
-    soFar = [];
-    userGuess;
+    $('#wins').empty();
+    $('#blank-spots').empty();
+    $('#guess-attempts').empty();
+    $('#letters-used').empty();
     computerChoice = alphabet[Math.floor(Math.random () * alphabet.length)];
 }
 
+// Randomizer function for generating random flag
+function randomizer(i){
+    let randomFlag = flagGuess[Math.floor(Math.random() * (flagGuess.length+1))].name;
+    if(previousFlag.includes(randomFlag) === true ){
+        randomizer()
+    } else {
+        previousFlag.push(randomFlag)
+        console.log(randomFlag)
+        return randomFlag
+    }
+};
 //On page load
 $(document).ready(function() {
 
-$('#doc').hide();
+
+    // flagGuess.forEach(randomizer)
+    randomizer()
+
+    // Function to randomly choose new flag
+    function randomizerThing(){
+
+        // flagGuess.forEach(function(){
+        //     let randomFlag = flagGuess
+        // })
+
+        // for(i in flagGuess[i]){
+        //     let randomFlag = Math.floor(Math.random*(flagGuess[i]+1));
+        // }
+        // return randomFlag
+    }
+    
+    let andorra = flagGuess[0].flagImage
+    $('#flag-column').append('<img src='+andorra+'/>')
+
+// $('#doc').hide();
 
 function startGame(){
 //This program will choose a random letter and the player will guess 
@@ -77,79 +108,13 @@ computerChoice = alphabet[Math.floor(Math.random () * alphabet.length)];
 console.log(computerChoice)
 
 // Game function 
-function gameLetter(letter) {
     
-    // String.fromCharCode method returns a string
-    userGuess = String.fromCharCode(letter.keyCode).toLowerCase();
-
-    soFar.push(userGuess);
-
-    document.getElementById('guessesSoFar').innerHTML = 'Your guesses so far: ' + soFar;
-
-    console.log(`The user has pressed the ${userGuess} key`);
-
-    // if (wins === 5){
-    //     document.getElementById('doc').innerHTML = 'You win'
-    // }
-    if(userGuess === computerChoice) {
-        // Log wins
-        wins++;
-
-        // Append the wins
-        document.getElementById('wins').innerHTML = 'Wins: ' + wins;
 
 
-        // if(wins = 2){
-        //     // Clear 
-        //     gameReset();
 
-
-        // }
-        // Again 
-        startGame();        
-    } 
-    // else if(guesses = 0){
-    //     losses++;
-    //     document.getElementById('losses').innerHTML = losses;
-
-    // } else {
-    //     guesses--;
-    //     document.getElementById('guessesLeft').innerHTML = guesses;
-    // }
+document.onkeypress = function(letter){
+    gameLetter(letter)
 }
-
-
-
-
-    document.onkeypress = function(letter){
-        gameLetter(letter)
-    }
 }
-
-startGame();
 
 });
-
-// Dom manipulation 
-
-// document.onkeypress = function(letter) {
-//     // const userGuess= innerHTML //how to get innerhtml from element ;
-//     userGuess = String.fromCharCode(letter.keyCode).toLowerCase();
-
-//     console.log(`the user has pressed the ${userGuess} key`);
-
-//     if (userGuess === computerChoice){
-//         wins++;
-//     }else{
-//         guesses--;
-//      }
-//      if (guesses === 0){
-//          losses ++;
-//      }
-//     document.getElementById('wins').innerHTML = "Wins:" + wins;
-//     document.getElementById('losses').innerHTML = "Losses:" + losses;
-//     document.getElementById('guessesleft').innerHTML = "Guesses left:" + guesses; 
-//  }; 
-
-//  let soFar= String.fromCharCode(letter).toLowerCase();
-//  document.getElementById('guessesSoFar').innerHTML = 'So far' + soFar; 
